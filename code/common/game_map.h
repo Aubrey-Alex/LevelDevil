8247c27e2cbb40ef937ceb9b3a252365c5f7eec6
Aubrey-Alex <1651256706@qq.com>
2025-06-30 16:42:19 +0800

    更改common/game_map.h,只保留Entity基类

--- code/common/game_map.h
+++ code/common/game_map.h
@@ -32,80 +32,58 @@ struct Entity {
     virtual ~Entity() = default;
 };
 
-// 玩家实体
-struct Player : public Entity {
-    float velocity_x = 0, velocity_y = 0;  // 速度
-    bool is_grounded = false;              // 是否在地面上
-    bool facing_right = true;              // 朝向
-    
-    Player() : Entity(EntityType::Player) {
-        physics.is_static = false;
-        physics.has_collision = true;
-        width = 32;  // 根据实际像素大小调整
-        height = 32;
-    }
-};
-
-// 墙实体
-struct Wall : public Entity {
-    Wall() : Entity(EntityType::Wall) {
-        physics.is_static = true;
-        physics.has_collision = true;
-    }
-};
-
-// 终点门实体
-struct Door : public Entity {
-    bool is_open = false;
-    
-    Door() : Entity(EntityType::Door) {
-        physics.is_static = true;
-        physics.is_trigger = true;
-        physics.has_collision = false;
-    }
-};
-
 // 游戏地图类
 class GameMap {
 public:
-    GameMap() = default;
-    GameMap(const GameMap&) = delete;
-    ~GameMap() = default;
-    
-    GameMap& operator=(const GameMap&) = delete;
-    
-    // 地图管理
-    void clear() { m_entities.clear(); }
-    size_t get_entity_count() const { return m_entities.size(); }
-    
-    // 实体访问
-    std::shared_ptr<Entity> get_entity(size_t idx) { 
-        return idx < m_entities.size() ? m_entities[idx] : nullptr; 
-    }
-    
-    // 实体管理
-    void add_entity(std::shared_ptr<Entity> entity) {
-        m_entities.push_back(entity);
-    }
-    
-    void remove_entity(size_t idx) {
-        if (idx < m_entities.size()) {
-            m_entities.erase(m_entities.begin() + idx);
-        }
-    }
-    
-    // 获取玩家实体
-    std::shared_ptr<Player> get_player() {
-        for (auto& entity : m_entities) {
-            if (entity->type == EntityType::Player) {
-                return std::static_pointer_cast<Player>(entity);
-            }
-        }
-        return nullptr;
-    }
+	GameMap() noexcept
+	{
+	}
+	GameMap(const GameMap&) = delete;
+	~GameMap() noexcept
+	{
+	}
+
+	GameMap& operator=(const GameMap&) = delete;
+
+	void clear() noexcept
+	{
+		m_entities.clear();
+	}
+
+	size_t get_size() const noexcept
+	{
+		return m_entities.size();
+	}
+
+	const Entity& get_at(size_t idx) const noexcept
+	{
+		return m_entities[idx];
+	}
+	Entity& get_at(size_t idx) noexcept
+	{
+		return m_entities[idx];
+	}
+
+	void set_size(size_t size)
+	{
+		m_entities.resize(size);
+	}
+
+	void append(const Entity& v)
+	{
+		m_entities.push_back(v);
+	}
+	void insert(size_t idx, const Entity& v)
+	{
+		m_entities.insert(m_entities.begin() + idx, v);
+	}
+	void remove(size_t idx) noexcept
+	{
+		m_entities.erase(m_entities.begin() + idx);
+	}
 
 private:
-    std::vector<std::shared_ptr<Entity>> m_entities;
+    std::vector<Entity> m_entities;
 };
 
 #endif
