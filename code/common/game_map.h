dd4046917a0ab98ff0e0a465bd025fd6e96a7f47
Tringmo <108381073+Tringmo@users.noreply.github.com>
2025-07-01 15:43:54 +0800

    Update game_map.h

--- code/common/game_map.h
+++ code/common/game_map.h
@@ -1,73 +1,48 @@
-#ifndef __GAME_MAP_H__
-#define __GAME_MAP_H__
+#ifndef GAME_MAP_H
+#define GAME_MAP_H
 
 #include <vector>
 #include <memory>
 #include "property_ids.h"
+#include "Entity.h"
 
-struct Position {
-	double x, y;
-	Position(double x, double y): x(x), y(y) {}
-};
-
-// 游戏实体基类
-class Entity {
-public:
-    char type;               // 实体类型
-    Position pos;           // 位置
-    double w, h;  // 大小
-    Entity(double x, double y): pos(x, y) {}
-};
-
-// 游戏地图类
 class GameMap {
 public:
-	GameMap() noexcept
-	{
-	}
-	GameMap(const GameMap&) = delete;
-	~GameMap() noexcept
-	{
-	}
+    GameMap() noexcept {}
+    GameMap(const GameMap&) = delete;
+    ~GameMap() noexcept {}
 
-	GameMap& operator=(const GameMap&) = delete;
+    GameMap& operator=(const GameMap&) = delete;
 
-	void clear() noexcept
-	{
-		m_entities.clear();
-	}
+    void clear() noexcept {
+        m_entities.clear();
+    }
 
-	size_t get_size() const noexcept
-	{
-		return m_entities.size();
-	}
+    size_t get_size() const noexcept {
+        return m_entities.size();
+    }
 
-	const Entity& get_at(size_t idx) const noexcept
-	{
-		return *m_entities[idx];
-	}
-	Entity& get_at(size_t idx) noexcept
-	{
-		return *m_entities[idx];
-	}
+    const Entity& get_at(size_t idx) const noexcept {
+        return *m_entities[idx];
+    }
+    Entity& get_at(size_t idx) noexcept {
+        return *m_entities[idx];
+    }
 
-	void set_size(size_t size)
-	{
-		m_entities.resize(size);
-	}
+    void set_size(size_t size) {
+        m_entities.resize(size);
+    }
 
-	void append(std::shared_ptr<Entity> v)
-	{
-		m_entities.push_back(v);
-	}
-	void insert(size_t idx, std::shared_ptr<Entity> v)
-	{
-		m_entities.insert(m_entities.begin() + idx, v);
-	}
-	void remove(size_t idx) noexcept
-	{
-		m_entities.erase(m_entities.begin() + idx);
-	}
+    void append(std::shared_ptr<Entity> v) {
+        m_entities.push_back(v);
+        rep(i, v->pos.x, v->pos.x+v->w-1) rep(j, v->pos.y, v->pos.y+v->h-1) mp[i][j]=v->type;
+    }
+    void insert(size_t idx, std::shared_ptr<Entity> v) {
+        m_entities.insert(m_entities.begin() + idx, v);
+    }
+    void remove(size_t idx) noexcept {
+        m_entities.erase(m_entities.begin() + idx);
+    }
 
 private:
     std::vector<std::shared_ptr<Entity>> m_entities;
